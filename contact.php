e1becf2a11d76293dd912f61f5761734f1ccf0ee
Cameron Scholes <cameron.scholes@hotmail.com>
2022-09-04 13:09:59 +0100

    Formatting bugs fixed

--- contact.php
+++ contact.php
@@ -3,53 +3,52 @@
 
 $selected = "contact";
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <title>Contact | Cameron Scholes</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/479b50a13e.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script type="text/javascript" src="scripts/nav.js"></script>
-    <script type="text/javascript" src="scripts/main.js"></script>
-  </head>
+<head>
+  <title>Contact | Cameron Scholes</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/479b50a13e.js" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/nav.js"></script>
+  <script type="text/javascript" src="scripts/main.js"></script>
+  <script>window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }</script>
+</head>
 
-  <body onload="mobileDeviceCheck();">
+<body>
 
-    <div class="background">
+  <div class="background">
     <?php include "include/header.php" ?>
     <main>
 
-      <div class="main">
 
-        <div class="contact-text-container">
+      <div class="contact-text-container">
 
-          <p class="contact-text">Unsure what you are looking for?<br>Need help building or optimising your website?</p>
-          <p class="contact-text">Get in contact today for a quote and I can share my years of knowledge!</p>
-
-        </div>
-
-        <form class="form shadow" action="include/contactForm.php" method="POST">
-          <h2 class="form-header">Get in contact</h2>
-          <h3 class="form-subheader text-muted">Lets discuss what you want!</h3>
-          <input class="contact-form-input form-control" type="text" placeholder="Name" name="Name">
-          <input class="contact-form-input form-control" type="email" placeholder="Email" name="Email">
-          <input class="contact-form-input form-control" type="tel" placeholder="Phone Number" name="Number">
-          <input class="contact-form-input form-control message" type="text" placeholder="Message..." name="Message">
-          <button class="contact-form-button btn btn-outline-info text-nowrap" >Send Message</button>
-
-        </form>
+        <p class="contact-text">Unsure what you are looking for?<br>Need help building or optimising your website?</p>
+        <p class="contact-text">Get in contact today for a quote and I can share my years of knowledge!</p>
 
       </div>
 
+      <form class="form shadow" action="include/contactForm.php" method="POST">
+        <h2 class="form-header">Get in contact</h2>
+        <h3 class="form-subheader text-muted">Lets discuss what you want!</h3>
+        <input class="contact-form-input form-control" type="text" placeholder="Name" name="Name">
+        <input class="contact-form-input form-control" type="email" placeholder="Email" name="Email">
+        <input class="contact-form-input form-control" type="tel" placeholder="Phone Number" name="Number">
+        <textarea class="form-control contact-form-input-message" type="text" placeholder="Message..." name="Message" rows="1"></textarea>
+        <button class="contact-form-button btn btn-outline-info text-nowrap contact-form-message" >Send Message</button>
+
+      </form>
     </main>
     <?php
     include "include/footer.php";
-     ?>
+    ?>
   </div>
-  </body>
+</body>
 </html>
--- include/header.php
+++ include/header.php
@@ -4,18 +4,16 @@ global $selected;
 
 ?>
 
-<nav>
-  <div class="nav-container">
+<nav class="nav-container">
     <div class="nav-item nav-exit" onclick="navClose()"><i class="fa-regular fa-circle-xmark fa-xs nav-item-exit"></i></div>
     <div class="nav-item"><a class="nav-link <?php if($selected == "home") echo "selected";?>" href="index.php">home</a><div class="nav-item-border"></div></div>
     <div class="nav-item"><a class="nav-link <?php if($selected == "portfolio") echo "selected";?>" href="portfolio.php">portfolio</a><div class="nav-item-border"></div></div>
     <div class="nav-item"><a class="nav-link <?php if($selected == "resume") echo "selected";?>" href="resume.php">resume</a><div class="nav-item-border"></div></div>
     <div class="nav-item"><a class="nav-link <?php if($selected == "contact") echo "selected";?>" href="contact.php">contact</a></div>
-  </div>
 </nav>
 <header>
 
-<div class="header">
+
 
   <h1 class="header-text">
     <a href="index.php" class="header-link"><span class="header-span">Cameron</span> Scholes</a>
@@ -24,6 +22,6 @@ global $selected;
   <div class="menu-button-container"><i class="fa-solid fa-chevron-down menu-button" onclick="navOpen()"></i></div>
   <div class="header-quote"><a href="contact.php">Get a quote</a> <i class="fa-regular fa-file-lines"></i></div>
 
-</div>
+
 
 </header>
